9cba8516a9a792d8c023087cc056a7937dacc30f
mm6104@nyu.edu <mmnjt4ever>
2017-02-20 23:49:23 -0800

    added tree info pt1

--- Algorithms/BinaryTreeTraversal.py
+++ Algorithms/BinaryTreeTraversal.py
@@ -3,3 +3,51 @@
 ######              Binary Tree Traversal             ######
 ############################################################
 
+# Inorder, Preorder, and Postorder binary tree traversal. (Variations of DFS.)
+
+# Assuming this data structure:
+# class BSTNode():
+#     def __init__(self, data=None, parent=None):
+#         self.data = data
+#         self.left = None
+#         self.right = None
+#         self.parent = None
+# class BSTree():
+#     def __init__(self):
+#         self.root = None
+
+############################################################
+
+def inorder(root):
+    def _inorder(node):
+        if node:
+            for node in _inorder(node.left):
+                yield node
+            yield node.data
+            for node in _inorder(node.right):
+                yield node
+    traversal = [node for node in _inorder(root)]
+    return traversal
+
+def preorder(root):
+    def _preorder(node):
+        if node:
+            yield node.data
+            for node in _preorder(node.left):
+                yield node
+            for node in _preorder(node.right):
+                yield node
+    traversal = [node for node in _preorder(root)]
+    return traversal
+
+def postorder(root):
+    def _postorder(node):
+        if node:
+            for node in _postorder(node.left):
+                yield node
+            for node in _postorder(node.right):
+                yield node
+            yield node.data
+    traversal = [node for node in _postorder(root)]
+    return traversal
+
